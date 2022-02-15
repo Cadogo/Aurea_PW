d698e81181dd7f0af24046b61eadccb276647b9a
carlos <carlos@flo.es>
2022-02-15 20:39:00 +0100

    Añadiendo favicon y descarga de apk

--- aureapw/src/screens/Descargar_App.js
+++ aureapw/src/screens/Descargar_App.js
@@ -8,7 +8,7 @@ const Descargar_App = () => {
     return (
         <div>
             <body id='contenedorCentrar'>
-                <h1 id='contenedorTituloDescargarApp'><a id='linkApp' href="https://www.youtube.com/watch?v=xvFZjo5PgG0&ab_channel=Duran">Descargar App</a></h1>
+                <h1 id='contenedorTituloDescargarApp'><a id='linkApp' href="https://drive.google.com/file/d/1sMh34_4lJMuLHPxLOiPTzn4KeR54WkOh/view?usp=sharing">Descargar App</a></h1>
                 <img id='qr' src={QR}></img>
             </body>
             <Footer />
